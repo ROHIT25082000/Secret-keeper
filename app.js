554c9e5e7975e6658d7817e5bcd206576e8911d9
Rohit <rv021561@gmail.com>
2020-07-30 18:31:40 +0530

    Added lodash

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+
+const _ = require('lodash');
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -34,11 +35,18 @@ app.get("/compose", function(req, res){
 	res.render("compose")
 });
 app.get("/posts/:other", function(req, res){
-	if(postsArray.includes(req.params.other)){
-		console.log("Match found !");
+	const requested = req.params.other;
+	for(var i = 0; i < postsArray.length; i++){
+		if(_.lowerCase(requested) === _.lowerCase(postsArray[i].title)){
+			console.log("Found here ");
+		}
+		else{
+			console.log("Not Found ");
+		}
 	}
+});
 
-})
+
 
 app.post("/compose" ,function(req, res){
 	const myPost = {};
